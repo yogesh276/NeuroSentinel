7cbdab28f98cdccccd8c9a7ee886777437c8febc
Yogesh <y816381@gmail.com>
2026-06-14 08:15:30 +0530

    Implement model training and saving for threat detection
    Added code to train a Random Forest model on threat data and save it as a pickle file.

--- train_model.py
+++ train_model.py
@@ -1,1 +1,16 @@
 
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+data = pd.read_csv("sample_logs.csv")
+
+X = data[['failed_logins','data_transfer_mb','unknown_process']]
+y = data['threat']
+
+model = RandomForestClassifier()
+model.fit(X,y)
+
+pickle.dump(model, open("threat_model.pkl","wb"))
+
+print("Model Trained")
